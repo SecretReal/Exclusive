6fee892a2f1a37bc3c5429ea8b3df8e355a47ca1
Secret <sakamikurumi@gmail.com>
2025-03-06 12:34:20 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,4 +4,6 @@ elseif game.GameId == 6754864595 then -- Moodeng fruit
 	loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/phi7tn207f745yi1i96820l0o65gz8jj.lua"))()
 elseif game.GameId == 6401952734 then -- Pets Go
 	loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/hyd24eirgb9d1n35o23a1ne0x6b3pdkg.lua"))()
+elseif game.GameId == 6793832056 then -- Rock Fruit
+	loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/li15i00tze7w1a49sta2wnl2qt3gn4w9.lua"))()
 end
