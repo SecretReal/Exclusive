0ed229caed16e87b002ecde5dcabb9ba54ba627e
Secret <sakamikurumi@gmail.com>
2025-03-06 13:52:14 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -6,4 +6,6 @@ elseif game.GameId == 6401952734 then -- Pets Go
 	loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/hyd24eirgb9d1n35o23a1ne0x6b3pdkg.lua"))()
 elseif game.GameId == 6793832056 then -- Rock Fruit
 	loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/li15i00tze7w1a49sta2wnl2qt3gn4w9.lua"))()
+elseif game.GameId == 6325068386 then -- Blue Lock: Rivals
+	loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/gqnu9srb19mmsvv4ts6mh5nodx885nnq.lua"))()
 end
