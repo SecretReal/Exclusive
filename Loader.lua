7723345957cacef90b0884fb2fd9223a0431cc34
SilverHub999 <patnarinnm@gmail.com>
2025-03-06 12:01:19 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,7 @@
 if game.GameId == 6471449680 then -- Verse Piece
-    loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/4icruao45zosa9rtzfrly8flfshh89gh.lua"))()
+	loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/4icruao45zosa9rtzfrly8flfshh89gh.lua"))()
+elseif game.GameId == 6754864595 then -- Moodeng fruit
+	loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/phi7tn207f745yi1i96820l0o65gz8jj.lua"))()
+elseif game.GameId == 6401952734 then -- Pets Go
+	loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/hyd24eirgb9d1n35o23a1ne0x6b3pdkg.lua"))()
 end
