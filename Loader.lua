22945ebd148c61713824346fd01e070a5e927931
SilverHub999 <patnarinnm@gmail.com>
2025-03-07 13:55:12 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,6 +4,8 @@ elseif game.GameId == 6754864595 then -- Moodeng fruit
 	loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/phi7tn207f745yi1i96820l0o65gz8jj.lua"))()
 elseif game.GameId == 6401952734 then -- Pets Go
 	loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/hyd24eirgb9d1n35o23a1ne0x6b3pdkg.lua"))()
+elseif game.GameId == 4509896324 then -- Anime Last Stand
+	loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/b6v70ptv0h5401l37itg2viy2t8zwcf4.lua"))()
 elseif game.GameId == 6793832056 then -- Rock Fruit
 	loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/li15i00tze7w1a49sta2wnl2qt3gn4w9.lua"))()
 elseif game.GameId == 6325068386 then -- Blue Lock: Rivals
