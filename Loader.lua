141f3750b7912bf479c4f19f0fcb3831d77fd916
Secret <sakamikurumi@gmail.com>
2025-06-15 00:26:57 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,34 @@
-loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/z2ioh2fx175gbqdsenk39xsy1dbjxjp3.lua"))()
+do
+	Players = game:GetService("Players")
+	Client = Players.LocalPlayer
+	__bl = {
+		"delta",
+		"solara",
+		"xeno",
+	}
+end
+
+repeat
+	task.wait()
+until game:IsLoaded()
+repeat
+	task.wait()
+until Client
+
+local Request
+if secure_get then
+	Request = game.secure_get
+else
+	Request = game.HttpGetAsync
+end
+
+if identifyexecutor then
+	for _, value in __bl do
+		if identifyexecutor():lower():find(value) then
+			Client:Kick("TH : ตัวรันกากไปเปลี่ยน / EN : Shitty Executor")
+			return
+		end
+	end
+end
+
+loadstring(Request(game, "https://api.realaya.xyz/v1/files/l/z2ioh2fx175gbqdsenk39xsy1dbjxjp3.lua"))()
