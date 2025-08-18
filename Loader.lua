520b44732b84f24508c933ba12d6e6f1a6a87677
Secret <sakamikurumi@gmail.com>
2025-08-18 16:13:35 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,7 @@
+repeat
+	task.wait()
+until game:IsLoaded() and game:GetService("Players").LocalPlayer
+
 do
 	Players = game:GetService("Players")
 	Client = Players.LocalPlayer
@@ -6,25 +10,11 @@ do
 		"solara",
 		"xeno",
 	}
-end
 
-repeat
-	task.wait()
-until game:IsLoaded()
-repeat
-	task.wait()
-until Client
-
-Client.Idled:Connect(function()
-    VirtualUser:CaptureController()
-    VirtualUser:ClickButton2(Vector2.new())
-end)
-
-local Request
-if secure_get then
-	Request = secure_get
-else
-	Request = game.HttpGetAsync
+	Client.Idled:Connect(function()
+		VirtualUser:CaptureController()
+		VirtualUser:ClickButton2(Vector2.new())
+	end)
 end
 
 if identifyexecutor then
@@ -36,4 +26,4 @@ if identifyexecutor then
 	end
 end
 
-loadstring(Request(game, "https://api.realaya.xyz/v1/files/l/z2ioh2fx175gbqdsenk39xsy1dbjxjp3.lua"))()
+loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/z2ioh2fx175gbqdsenk39xsy1dbjxjp3.lua"))()
