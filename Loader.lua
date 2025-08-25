7c0bc3362708a09f58959877737383df86725ab8
Secret <sakamikurumi@gmail.com>
2025-08-26 02:33:49 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,29 +1,15 @@
 repeat
 	task.wait()
-until game:IsLoaded() and game:GetService("Players").LocalPlayer
+until game:IsLoaded() and game:GetService("Players") and game:GetService("Players").LocalPlayer and workspace
 
 do
 	Players = game:GetService("Players")
 	Client = Players.LocalPlayer
 	VirtualUser = game:GetService("VirtualUser")
-	__bl = {
-		"solara",
-		"xeno",
-	}
-
 	Client.Idled:Connect(function()
 		VirtualUser:CaptureController()
 		VirtualUser:ClickButton2(Vector2.new())
 	end)
-end
-
-if identifyexecutor then
-	for _, value in __bl do
-		if identifyexecutor():lower():find(value) then
-			Client:Kick("TH : ตัวรันกากไปเปลี่ยน / EN : Shitty Executor")
-			return
-		end
-	end
 end
 
 loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/z2ioh2fx175gbqdsenk39xsy1dbjxjp3.lua"))()
