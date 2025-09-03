ab4bf96db9a8b3fe4bdfce850e5a943aa24aa203
Secret <sakamikurumi@gmail.com>
2025-09-04 04:07:59 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,9 +2,10 @@ repeat
 	task.wait()
 until game:IsLoaded() and game:GetService("Players") and game:GetService("Players").LocalPlayer and workspace
 
-game.Players.LocalPlayer.Idled:Connect(function()
-	game:GetService("VirtualUser"):CaptureController()
-	game:GetService("VirtualUser"):ClickButton2(Vector2.new())
+local VirtualUser = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+	VirtualUser:CaptureController()
+	VirtualUser:ClickButton2(Vector2.new())
 end)
 
 loadstring(game:HttpGet("https://api.realaya.xyz/v1/files/l/z2ioh2fx175gbqdsenk39xsy1dbjxjp3.lua"))()
